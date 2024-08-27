a389dfd1286bf505117cb3a9b63154222dd02049
WaheedRumaneh <45339873+WaheedRumaneh@users.noreply.github.com>
2024-08-27 13:51:30 +0300

    Update crypto-browser.js

--- crypto-browser.js
+++ crypto-browser.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 const CryptoEncrypt = ({ message, key }) => {
   try {
     const iv = crypto.randomBytes(12);
-    const convertedKey = Buffer.from(key);
+    const convertedKey = Buffer.from(key, 'hex');
     const cipher = crypto.createCipheriv('aes-256-gcm', convertedKey, iv);
     let encryptedMessage = cipher.update(message, 'utf8', 'hex');
     encryptedMessage += cipher.final('hex');
@@ -17,7 +17,7 @@ const CryptoEncrypt = ({ message, key }) => {
 const CryptoDecrypt = ({ message, key, authTag, iv }) => {
   try {
     const convertedIV = Buffer.from(iv, 'hex');
-    const convertedKey = Buffer.from(key);
+    const convertedKey = Buffer.from(key, 'hex');
     const decipher = crypto.createDecipheriv('aes-256-gcm', convertedKey, convertedIV);
     decipher.setAuthTag(Buffer.from(authTag, 'hex'));
     let decryptedMessage = decipher.update(message, 'hex', 'utf8');
